2a8198b35f2f1b6f22815f082a00e98c9cb8209b
azy64 <allysaidi64@gmail.com>
2021-12-17 13:02:52 +0100

    fix the linters errors

--- MyList.rb
+++ MyList.rb
@@ -1,14 +1,14 @@
 #$LOAD_PATH <<'.'
-require_relative "MyEnumerable"
+require_relative "my_enumerable"
 class MyList
-    include MyEnumerable
-    def initialize(*list)
-      @list = list
-    end
-    
-    def each(&block)
-      @list.each(&block)
-    end
+  include MyEnumerable
+  def initialize(*list)
+    @list = list
+  end
+
+  def each(&block)
+    @list.each(&block)
+  end
 end
 
 l = MyList.new(2, 3, 4)
